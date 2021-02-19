f0ac88d15234b250be3de912bbdde756e8984efd
mhar <lgusanfernando@gmail.com>
2021-02-19 10:19:37 +0800

    updated

---
+++ appserver/etracs/modules/rameses-gov-etracs-ctc-services.jar/sql/ctcreport.sql
@@ -0,0 +1,42 @@
+[getAbstractCTCIndividual]
+SELECT
+	r.txnno,
+	r.dtposted as txndate,
+	c.receiptno AS orno,
+	CASE WHEN cv.objid IS null THEN c.receiptdate ELSE NULL END AS ordate,
+	CASE WHEN cv.objid IS null THEN c.paidby ELSE '*** VOIDED ***' END AS paidby,
+	CASE WHEN cv.objid IS null THEN c.amount ELSE 0.0 END AS amount,
+	r.collector_name as collectorname,
+	r.collector_title as  collectortitle,
+	r.liquidatingofficer_name as liquidatingofficername,
+	r.liquidatingofficer_title as liquidatingofficertitle,
+	CASE WHEN cv.objid IS null THEN 0 else 1 end as voided,
+	ctci.basictax as basic, 
+	(ctci.salarytax + ctci.propertyincometax + ctci.businessgrosstax + ctci.additionaltax) as additional,
+	ctci.interestdue as penalty,
+	ctci.amountdue as total 
+FROM remittance r
+	INNER JOIN remittance_cashreceipt rc ON rc.remittanceid = r.objid
+	INNER JOIN cashreceipt  c on c.objid = rc.objid 
+	INNER JOIN cashreceipt_ctc_individual ctci on ctci.objid = c.objid 
+	LEFT JOIN cashreceipt_void cv on cv.receiptid = c.objid 
+WHERE r.objid = $P{objid}
+  AND c.formno = '0016' 
+
+
+[getTaxRoll]
+select 
+	rem.remittancedate, rem.txnno, rem.collector_name, 
+	cr.paidby, cr.paidbyaddress, cr.receiptdate, cr.receiptno, ei.birthdate, 
+	ctci.basictax, ctci.additionaltax, ctci.interest, 
+	(ctci.basictax + ctci.additionaltax + ctci.interest) as total, 
+	cr.objid as receiptid 
+from remittance rem 
+	inner join remittance_cashreceipt remc on rem.objid=remc.remittanceid
+	inner join cashreceipt cr on remc.objid=cr.objid 
+	inner join cashreceipt_ctc_individual ctci on cr.objid=ctci.objid 
+	left join entityindividual ei on cr.payer_objid=ei.objid 
+where rem.remittancedate >= $P{startdate} 
+	and rem.remittancedate < $P{enddate} 
+	and cr.formno='0016' 
+order by rem.remittancedate, rem.collector_name, cr.series   
